a173e6e98ffaec384892e51cc811ca1ba60790ab
pivakvadym <pivak.vadym@gmail.com>
2014-12-25 22:30:35 +0100

    123

---
+++ ServerGameB.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Vadym on 23/12/14.
+ */
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200);
+    res.end('Salut tout le monde !');
+});
+server.listen(8080);
